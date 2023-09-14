e3b8d395e8e9eb74fe996410b4aaf12eedf59b65
Francisco Cerqueira <up201905337@fe.up.pt>
2023-09-14 14:59:12 +0100

    chore: change to relative imports in loader.py

--- src/utils/loader.py
+++ src/utils/loader.py
@@ -1,8 +1,8 @@
 import yaml
 import os
-import src.core as core
-import src.datasets as datasets
-import src.models as models
+from ..core import classes as core_classes
+from ..datasets import classes as datasets_classes
+from ..models import classes as models_classes
 from .constants import Constants as c
 
 
@@ -30,36 +30,36 @@ class Loader:
         return config
 
     def load_dataset(self, name: str):
-        return self._load_config(c.Loader.DATASETS_CONFIG_FILENAME, name, datasets.classes)
+        return self._load_config(c.Loader.DATASETS_CONFIG_FILENAME, name, datasets_classes)
 
     def load_model(self, name: str):
-        return self._load_config(c.Loader.MODELS_CONFIG_FILENAME, name, models.classes)
+        return self._load_config(c.Loader.MODELS_CONFIG_FILENAME, name, models_classes)
 
     def load_loss(self, name: str):
-        return self._load_config(c.Loader.LOSSES_CONFIG_FILENAME, name, core.classes["losses"])
+        return self._load_config(c.Loader.LOSSES_CONFIG_FILENAME, name, core_classes["losses"])
 
     def load_metrics(self, names: list):
         metrics_dict = {}
         metrics_configs_dict = {}
         for name in names:
             metrics_dict[name], metrics_configs_dict[name] = self._load_config(
-                c.Loader.METRICS_CONFIG_FILENAME, name, core.classes["metrics"]
+                c.Loader.METRICS_CONFIG_FILENAME, name, core_classes["metrics"]
             )
         return metrics_dict, metrics_configs_dict
 
     def load_optimizer(self, name: str, model):
         return self._load_config(
-            c.Loader.OPTIMIZERS_CONFIG_FILENAME, name, core.classes["optimizers"], {"params": model.parameters()}
+            c.Loader.OPTIMIZERS_CONFIG_FILENAME, name, core_classes["optimizers"], {"params": model.parameters()}
         )
 
     def load_scheduler(self, name: str, optimizer):
-        if name == None:
+        if name is None:
             return None, None
         return self._load_config(
-            c.Loader.SCHEDULERS_CONFIG_FILENAME, name, core.classes["schedulers"], {"optimizer": optimizer}
+            c.Loader.SCHEDULERS_CONFIG_FILENAME, name, core_classes["schedulers"], {"optimizer": optimizer}
         )
 
     def load_stop_condition(self, name: str):
-        if name == None:
+        if name is None:
             return None, None
-        return self._load_config(c.Loader.STOP_CONDITIONS_CONFIG_FILENAME, name, core.classes["stop_conditions"])
+        return self._load_config(c.Loader.STOP_CONDITIONS_CONFIG_FILENAME, name, core_classes["stop_conditions"])
